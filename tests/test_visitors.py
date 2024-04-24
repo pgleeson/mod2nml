e258b029517d21aff5f376f12ffde2332c0ba9d4
Boris Marin <borismarin@gmail.com>
2024-04-24 03:18:23 -0300

    first tests

---
+++ tests/test_visitors.py
@@ -0,0 +1,21 @@
+import nmodl.dsl as nmodl
+from nmodl import ast, visitor
+
+
+with open("sample_mods/nap.mod") as f:
+    mod = f.read()
+
+driver = nmodl.NmodlDriver()
+modast = driver.parse_string(mod)
+
+lookup_visitor = visitor.AstLookupVisitor()
+
+def test_useion():
+    useions = lookup_visitor.lookup(modast, ast.AstNodeType.USEION)
+    for useion in useions:
+        print('using ion', useion.name)
+
+def test_states():
+    states = lookup_visitor.lookup(modast, ast.AstNodeType.STATE_BLOCK)
+    for state in states:
+        print(nmodl.to_nmodl(state))
